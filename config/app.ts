d227c5ef9e169b5b0c2b9edc9129872f373408c9
razlevio <razlevio.55@gmail.com>
2025-02-14 18:12:52 +0200

    fix(seo): configuration to the way google render the website

--- config/app.ts
+++ config/app.ts
@@ -1,8 +1,8 @@
 export const appConfig = {
-	name: "Raz Levi",
-	description: "Building software products that fit seamlessly into life 🚀",
-	url: "https://razlevio.com",
-	ogImage: "https://razlevio.com/avatar.jpeg",
+  name: "Raz Levi",
+  description: "Building software products that fit seamlessly into life 🚀",
+  url: "https://razlevio.com",
+  ogImage: "https://razlevio.com/avatar.jpeg",
 };
 
 export type AppConfig = typeof appConfig;
